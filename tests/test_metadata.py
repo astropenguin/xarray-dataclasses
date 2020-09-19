62b8234d8587dddd5d797728a2e63160b8c148f5
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-09-19 18:09:35 +0900

    Reset package version

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -6,4 +6,4 @@ def test_author():
 
 
 def test_version():
-    assert demo.__version__ == "0.2.0"
+    assert demo.__version__ == "0.1.0"
--- xarray_dataclasses/__init__.py
+++ xarray_dataclasses/__init__.py
@@ -1,2 +1,2 @@
 __author__ = "Akio Taniguchi"
-__version__ = "0.2.0"
+__version__ = "0.1.0"
