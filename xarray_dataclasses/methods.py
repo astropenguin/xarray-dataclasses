7c79bb935b12e62cb858b7f9efa38b76bac11025
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-12-09 14:46:55 +0900

    #9 Add methods module

---
+++ xarray_dataclasses/methods.py
@@ -0,0 +1,53 @@
+# standard library
+from typing import Any, Optional, Sequence, Union
+
+
+# dependencies
+import numpy as np
+from numpy import ndarray
+from typing_extensions import Literal
+
+
+# constants
+C: str = "C"
+F: str = "F"
+
+
+# type aliases
+Shape = Sequence[int]
+Dtype = Optional[Union[type, str]]
+Order = Literal[C, F]
+
+
+# main features
+def empty(
+    shape: Shape,
+    dtype: Dtype = None,
+    order: Order = C,
+) -> ndarray:
+    return np.empty(shape, dtype, order)
+
+
+def zeros(
+    shape: Shape,
+    dtype: Dtype = None,
+    order: Order = C,
+) -> ndarray:
+    return np.zeros(shape, dtype, order)
+
+
+def ones(
+    shape: Shape,
+    dtype: Dtype = None,
+    order: Order = C,
+) -> ndarray:
+    return np.ones(shape, dtype, order)
+
+
+def full(
+    shape: Shape,
+    fill_value: Any,
+    dtype: Dtype = None,
+    order: Order = C,
+) -> ndarray:
+    return np.full(shape, fill_value, dtype, order)
