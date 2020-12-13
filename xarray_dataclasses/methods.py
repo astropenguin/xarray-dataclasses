bbaa4f789880e974148b0f4f0a6493c33f92e6ce
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-12-13 16:06:21 +0900

    #14 Update functions in methods module to return DataArray instances

--- xarray_dataclasses/methods.py
+++ xarray_dataclasses/methods.py
@@ -1,4 +1,4 @@
-__all__ = ["empty", "zeros", "ones", "full"]
+__all__ = ["new", "empty", "zeros", "ones", "full"]
 
 
 # standard library
@@ -7,7 +7,9 @@ from typing import Any, Optional, Sequence, Union
 
 # dependencies
 import numpy as np
+import xarray as xr
 from typing_extensions import Literal
+from .typing import Name, Attrs
 
 
 # constants
@@ -22,34 +24,56 @@ Order = Literal[C, F]
 
 
 # main features
+def new(
+    data: Any,
+    *,
+    name: Name = None,
+    attrs: Attrs = None,
+) -> xr.DataArray:
+    return xr.DataArray(data, name=name, attrs=attrs)
+
+
 def empty(
     shape: Shape,
+    *,
     dtype: Dtype = None,
     order: Order = C,
-) -> np.ndarray:
-    return np.empty(shape, dtype, order)
+    name: Name = None,
+    attrs: Attrs = None,
+) -> xr.DataArray:
+    return new(np.empty(shape, dtype, order), name, attrs)
 
 
 def zeros(
     shape: Shape,
+    *,
     dtype: Dtype = None,
     order: Order = C,
-) -> np.ndarray:
-    return np.zeros(shape, dtype, order)
+    name: Name = None,
+    attrs: Attrs = None,
+) -> xr.DataArray:
+    return new(np.zeros(shape, dtype, order), name, attrs)
 
 
 def ones(
     shape: Shape,
+    *,
     dtype: Dtype = None,
     order: Order = C,
-) -> np.ndarray:
+    name: Name = None,
+    attrs: Attrs = None,
+) -> xr.DataArray:
     return np.ones(shape, dtype, order)
+    return new(np.ones(shape, dtype, order), name, attrs)
 
 
 def full(
     shape: Shape,
     fill_value: Any,
+    *,
     dtype: Dtype = None,
     order: Order = C,
-) -> np.ndarray:
-    return np.full(shape, fill_value, dtype, order)
+    name: Name = None,
+    attrs: Attrs = None,
+) -> xr.DataArray:
+    return new(np.full(shape, fill_value, dtype, order), name, attrs)
