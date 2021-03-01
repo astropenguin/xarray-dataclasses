654c62b0b5ccfa041f8621bd45d5d98e3fed14dc
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-01 18:19:31 +0900

    #17 Add asdataarray function

--- xarray_dataclasses/core.py
+++ xarray_dataclasses/core.py
@@ -1,14 +1,16 @@
-__all__ = ["dataarrayclass"]
+__all__ = ["dataarrayclass", "asdataarray"]
 
 
 # standard library
 from dataclasses import dataclass, Field, _DataclassParams
-from typing import Callable, Dict, Optional, Union
+from typing import Any, Callable, Dict, Optional, Union
 
 
 # third-party packages
+import numpy as np
+import xarray as xr
 from typing_extensions import Protocol
-from .field import set_fields
+from .field import Kind, set_fields
 
 
 # type hints
@@ -53,3 +55,38 @@ def dataarrayclass(
         return to_dataclass(cls)  # DataClass
     else:
         return to_dataclass  # DataClassDecorator
+
+
+def asdataarray(obj: DataClass) -> xr.DataArray:
+    """Convert dataclass instance to a DataArray instance."""
+    fields = obj.__dataclass_fields__
+    dataarray = fields["data"].type(obj.data)
+
+    for field in fields.values():
+        value = getattr(obj, field.name)
+        set_value(dataarray, field, value)
+
+    return dataarray
+
+
+# helper features
+def set_value(dataarray: xr.DataArray, field: Field, value: Any) -> xr.DataArray:
+    """Set value to a DataArray instance according to given field."""
+    kind = field.metadata["xarray"].kind
+
+    if kind == Kind.ATTR:
+        dataarray.attrs[field.name] = value
+    elif kind == Kind.COORD:
+        try:
+            coord = field.type(value)
+        except ValueError:
+            shape = tuple(dataarray.sizes[dim] for dim in field.type.dims)
+            coord = field.type(np.full(shape, value))
+        finally:
+            dataarray.coords[field.name] = coord
+    elif kind == Kind.DATA:
+        pass
+    elif kind == Kind.NAME:
+        dataarray.name = value
+
+    return dataarray
