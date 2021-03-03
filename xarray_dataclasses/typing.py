05051a31572279c6964a597fc0f256f222abb6d6
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-04 00:07:11 +0900

    #26 Update typing module (refactoring)

--- xarray_dataclasses/typing.py
+++ xarray_dataclasses/typing.py
@@ -3,17 +3,7 @@ __all__ = ["DataArray"]
 
 # standard library
 from dataclasses import Field, _DataclassParams
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    Hashable,
-    Mapping,
-    Optional,
-    Sequence,
-    Tuple,
-    Union,
-)
+from typing import Any, Callable, Dict, Optional, Tuple, Sequence, Union
 
 
 # third-party packages
@@ -40,16 +30,14 @@ Shape = Union[Sequence[int], int]
 
 
 # type hints (xarray)
-Attrs = Optional[Mapping]
-Coords = Optional[Union[Sequence[Tuple], Mapping[Hashable, Any]]]
-Dims = Optional[Union[Sequence[Hashable], Hashable]]
-Name = Optional[Hashable]
+Hints = type("Hints", (), xr.DataArray.__init__.__annotations__)
 
 
 class DataArrayMeta(type):
-    """Metaclass of the type hint for xarray.DataArray."""
+    """Metaclass of the type hint for DataArray."""
 
-    def __getitem__(cls, options: Tuple[Dims, Dtype]) -> "DataArrayMeta":
+    def __getitem__(cls, options: Tuple[Hints.dims, Dtype]) -> "DataArrayMeta":
+        """Define the behavior of DataArray[dims, dtype]."""
         try:
             dims, dtype = options
         except (ValueError, TypeError):
@@ -71,10 +59,10 @@ class DataArrayMeta(type):
 
         namespace = cls.__dict__.copy()
         namespace.update(dims=dims, dtype=dtype)
-
         return DataArrayMeta(name, (cls,), namespace)
 
     def __instancecheck__(cls, inst: Any) -> bool:
+        """Define the behavior of isinstance(inst, DataArray)."""
         if not isinstance(inst, xr.DataArray):
             return False
 
@@ -92,65 +80,20 @@ class DataArrayMeta(type):
 
 
 class DataArray(xr.DataArray, metaclass=DataArrayMeta):
-    """Type hint for xarray.DataArray.
+    """Type hint for xarray.DataArray."""
 
-    As shown in the examples, it enables to specify fixed dimension(s)
-    (``dims``) and datatype (``dtype``) of ``xarray.DataArray``.
-    Users can use it to create a ``DataArray`` instance with fixed
-    dimension(s) and datatype in the same manner as ``xarray.DataArray``.
-
-    Args:
-        data: Values of a ``DataArray`` instance.
-            They are cast to ``dtype`` if it is specified in a hint.
-        coords: Coordinates of a ``DataArray`` instance.
-        dims: Dimension(s) of a ``DataArray`` instance.
-            It is ignored if ``dims`` is specified in a hint.
-        name: Name of a ``DataArray`` instance.
-        attrs: Attributes of a ``DataArray`` instance.
-
-    Returns:
-        ``DataArray`` instance with fixed ``dims`` and ``dtype``.
-
-    Examples:
-        To fix ``dims`` to be ``('x', 'y')``::
-
-            DataArray[('x', 'y'), None]
-
-        To fix ``dtype`` to be ``float``::
-
-            DataArray[None, float]
-
-        To fix both ``dims`` and ``dtype``::
-
-            DataArray[('x', 'y'), float]
-
-        Not to fix neither ``dims`` nor ``dtype``::
-
-            DataArray # or DataArray[None, None]
-
-        A type can be instantiated::
-
-            DataArray["x", float]([0, 1, 2])
-
-            # <xarray.DataArray (x: 3)>
-            # array([0., 1., 2.])
-            # Dimensions without coordinates: x
-
-    """
-
-    __slots__: Tuple[str, ...] = ()  #: Do not allow to add any values.
-    dims: Dims = None  #: Dimensions to be fixed in DataArray instances.
-    dtype: Dtype = None  #: Datatype to be fixed in DataArray instances.
+    dims: Hints.Dims = None
+    dtype: Dtype = None
+    __slots__: Tuple[()] = ()
 
     def __new__(
         cls,
-        data: Any,
-        coords: Coords = None,
-        dims: Dims = None,
-        name: Name = None,
-        attrs: Attrs = None,
+        data: Hints.data,
+        coords: Hints.coords = None,
+        dims: Hints.dims = None,
+        name: Hints.name = None,
+        attrs: Hints.attrs = None,
     ) -> xr.DataArray:
-        """Create a DataArray instance with fixed dims and dtype."""
         data = np.array(data, cls.dtype)
-        dims = dims if cls.dims is None else cls.dims
+        dims = cls.dims if cls.dims is not None else dims
         return xr.DataArray(data, coords, dims, name, attrs)
