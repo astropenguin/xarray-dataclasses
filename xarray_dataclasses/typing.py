b7871a6a7750ad4b69c0f93ec511e36ffacc687c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-07 21:53:59 +0900

    #26 Update typing module

--- xarray_dataclasses/typing.py
+++ xarray_dataclasses/typing.py
@@ -1,123 +1,151 @@
-from __future__ import annotations
-
-
 __all__ = ["Attr", "Coord", "Data", "Name"]
 
 
 # standard library
-from dataclasses import Field, _DataclassParams
-from typing import Any, Callable, Dict, Generic, Tuple, TypeVar, Union
+from enum import auto, Enum
+from functools import wraps
+from typing import Generic, Sequence, TypeVar, Union
 
 
 # third-party packages
 import numpy as np
 import xarray as xr
-from typing_extensions import Protocol
+from typing_extensions import Annotated
 
 
-# type hints (dataclasses)
-class DataClass(Protocol):
-    """Type hint for dataclass and its instance."""
+# constants
+class Xarray(Enum):
+    """Identification for public type hints."""
 
-    __dataclass_fields__: Dict[str, Field]
-    __dataclass_params__: _DataclassParams
-
-
-DataClassDecorator = Callable[[type], DataClass]
-"""Type hint for decorator that generates a dataclass."""
+    ATTR = auto()  #: Attribute member of DataArray or Dataset.
+    COORD = auto()  #: Coordinate member of DataArray or Dataset.
+    DATA = auto()  #: Data of DataArray or variable of Dataset.
+    NAME = auto()  #: Name of DataArray.
 
 
-# type hints (xarray)
-T = TypeVar("T")
-Dtype = Union[np.dtype, type, str, None]
-Hints = type("Hints", (), xr.DataArray.__init__.__annotations__)
+# type variables
+T = TypeVar("T")  #: Type variable for data types.
+D = TypeVar("D")  #: Type valiable for dimensions.
 
 
-class DataArrayMeta(type):
-    """Metaclass of the type hint for DataArray."""
+# type hints (internal)
+class ndarray(Generic[T]):
+    """Generic version of numpy.ndarray."""
 
-    def __getitem__(cls, key: Tuple[Hints.dims, Dtype]) -> DataArrayMeta:
-        """Define the behavior of DataArray[dims, dtype]."""
-        try:
-            dims, dtype = key
-        except (ValueError, TypeError):
-            raise ValueError("Both dims and dtype must be specified.")
-
-        if isinstance(dims, str):
-            dims = (dims,)
-
-        if dims is not None:
-            dims = tuple(dims)
-
-        if dtype is not None:
-            dtype = np.dtype(dtype)
-
-        if dims is None and dtype is None:
-            name = cls.__name__
-        else:
-            name = f"{cls.__name__}[{dims!s}, {dtype!s}]"
-
-        namespace = cls.__dict__.copy()
-        namespace.update(dims=dims, dtype=dtype)
-        return DataArrayMeta(name, (cls,), namespace)
-
-    def __instancecheck__(cls, obj: Any) -> bool:
-        """Define the behavior of isinstance(obj, DataArray)."""
-        if not isinstance(obj, xr.DataArray):
-            return False
-
-        if cls.dims is None:
-            is_equal_dims = True  # Do not evaluate.
-        else:
-            is_equal_dims = obj.dims == cls.dims
-
-        if cls.dtype is None:
-            is_equal_dtype = True  # Do not evaluate.
-        else:
-            is_equal_dtype = obj.dtype == cls.dtype
-
-        return is_equal_dims and is_equal_dtype
+    @wraps(np.ndarray)
+    def __new__(cls, *args, **kwargs):
+        return np.ndarray(*args, **kwargs)
 
 
-class DataArray(xr.DataArray, metaclass=DataArrayMeta):
-    """Type hint for xarray.DataArray."""
+class DataArray(Generic[T, D]):
+    """Generic version of xarray.DataArray."""
 
-    dims: Hints.Dims = None
-    dtype: Dtype = None
-    __slots__ = ()
-
-    def __new__(
-        cls,
-        data: Hints.data,
-        coords: Hints.coords = None,
-        dims: Hints.dims = None,
-        name: Hints.name = None,
-        attrs: Hints.attrs = None,
-    ) -> xr.DataArray:
-        data = np.array(data, cls.dtype)
-        dims = cls.dims if cls.dims is not None else dims
-        return xr.DataArray(data, coords, dims, name, attrs)
+    @wraps(xr.DataArray)
+    def __new__(cls, *args, **kwargs):
+        return xr.DataArray(*args, **kwargs)
 
 
-class Coord(DataArray):
-    """Type hint for coordinates of DataArray."""
-
-    __slots__ = ()
+DataArrayLike = Union[DataArray[T, D], ndarray[T], Sequence[T], T]
 
 
-class Data(DataArray):
-    """Type hint for data variables of DataArray."""
+# type hints (public)
+Attr = Annotated[T, Xarray.ATTR]
+"""Type hint for attribute member of DataArray or Dataset.
 
-    __slots__ = ()
+Examples:
+    ::
+
+        from typing import Literal
+        from xarray_dataclasses import dataarrayclass, Data, Attr
 
 
-class Attr(Generic[T]):
-    """Type hint for attrs members of DataArray."""
-
-    pass
+        X = Literal["x"]
+        Y = Literal["y"]
 
 
-class Name(Generic[T]):
-    """Type hint for name of DataArray."""
+        @dataarrayclass
+        class Image:
+            data: Data[float, tuple[X, Y]]
+            dpi: Attr[int] = 300
 
-    pass
+"""
+
+Coord = Annotated[DataArrayLike[T, D], Xarray.COORD]
+"""Type hint for coordinate member of DataArray or Dataset.
+
+Examples:
+    ::
+
+        from typing import Literal
+        from xarray_dataclasses import dataarrayclass, Data, Coord
+
+
+        X = Literal["x"]
+        Y = Literal["y"]
+
+
+        @dataarrayclass
+        class Image:
+            data: Data[float, tuple[X, Y]]
+            weight: Coord[float, tuple[X, Y]] = 1.0
+            x: Coord[int, X] = 0
+            y: Coord[int, Y] = 0
+
+"""
+
+Data = Annotated[DataArrayLike[T, D], Xarray.DATA]
+"""Type hint for data of DataArray or variable of Dataset.
+
+Examples:
+    ::
+
+        from typing import Literal
+        from xarray_dataclasses import dataarrayclass, Data
+
+
+        X = Literal["x"]
+        Y = Literal["y"]
+
+
+        @dataarrayclass
+        class Image:
+            data: Data[float, tuple[X, Y]]
+
+    ::
+
+        from typing import Literal
+        from xarray_dataclasses import datasetclass, Data
+
+
+        X = Literal["x"]
+        Y = Literal["y"]
+
+
+        @datasetclass
+        class Images:
+            red: Data[float, tuple[X, Y]]
+            green: Data[float, tuple[X, Y]]
+            blue: Data[float, tuple[X, Y]]
+
+"""
+
+Name = Annotated[T, Xarray.NAME]
+"""Type hint for name of DataArray.
+
+Examples:
+    ::
+
+        from typing import Literal
+        from xarray_dataclasses import dataarrayclass, Data, Name
+
+
+        X = Literal["x"]
+        Y = Literal["y"]
+
+
+        @dataarrayclass
+        class Image:
+            data: Data[float, tuple[X, Y]]
+            name: Name[str] = "default"
+
+"""
