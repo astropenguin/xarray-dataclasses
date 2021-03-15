d23456ac5d94cb32473ee9f3fec85890092baddd
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-16 00:14:41 +0900

    #26 Update tests for typing module

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,66 +1,58 @@
+# standard library
+from typing import Any, Tuple
+
+
 # third-party packages
 import numpy as np
-import xarray as xr
 from pytest import mark
-from xarray_dataclasses.typing import DataArray
+from typing_extensions import Literal
+
+
+# submodules
+from xarray_dataclasses.typing import DataArrayLike, get_dims, get_dtype
+
+
+# type hints
+X = Literal["x"]
+Y = Literal["y"]
 
 
 # test datasets
-dataarray = xr.DataArray([1, 2, 3], dims="x")
-
 testdata_dims = [
-    (None, None),
-    ((), ()),
+    (None, ()),
+    (Tuple[()], ()),
+    (X, ("x",)),
     ("x", ("x",)),
-    (("x",), ("x",)),
-    (("x", "y"), ("x", "y")),
+    (Tuple[X], ("x",)),
+    (Tuple["x"], ("x",)),  # noqa
+    (Tuple[X, Y], ("x", "y")),
+    (Tuple["x", "y"], ("x", "y")),  # noqa
 ]
 
 testdata_dtype = [
-    (None, None),
+    (Any, None),
+    (bool, np.bool_),
+    (bytes, np.bytes_),
+    (str, np.str_),
     (int, np.int64),
     (float, np.float64),
+    (complex, np.complex128),
+    ("bool", np.bool_),
+    ("bytes", np.bytes_),
+    ("str", np.str_),
     ("int", np.int64),
     ("float", np.float64),
-]
-
-testdata_isinstance = [
-    (dataarray, DataArray, True),
-    (dataarray, DataArray[None, None], True),
-    (dataarray, DataArray[None, int], True),
-    (dataarray, DataArray[None, float], False),
-    (dataarray, DataArray["x", None], True),
-    (dataarray, DataArray["y", None], False),
-    (dataarray, DataArray["x", int], True),
-    (dataarray, DataArray["y", float], False),
-]
-
-testdata_issubclass = [
-    (DataArray, xr.DataArray, True),
-    (DataArray["x", None], xr.DataArray, True),
-    (DataArray["x", None], DataArray, True),
-    (xr.DataArray, DataArray, False),
-    (xr.DataArray, DataArray["x", None], False),
-    (DataArray, DataArray["x", None], False),
+    ("complex", np.complex128),
+    ("datetime64", np.datetime64),
 ]
 
 
 # test functions
-@mark.parametrize("dims, expected", testdata_dims)
-def test_dims(dims, expected):
-    assert DataArray[dims, None].dims == expected
+@mark.parametrize("test, expected", testdata_dims)
+def test_dims(dims: Any, expected: Any) -> None:
+    assert get_dims(DataArrayLike[Any, dims]) == expected  # type: ignore
 
 
 @mark.parametrize("dtype, expected", testdata_dtype)
-def test_dtype(dtype, expected):
-    assert DataArray[None, dtype].dtype == expected
-
-
-@mark.parametrize("dataarray, type_, expected", testdata_isinstance)
-def test_isinstance(dataarray, type_, expected):
-    assert isinstance(dataarray, type_) == expected
-
-
-@mark.parametrize("subcls, cls, expected", testdata_issubclass)
-def test_issubclass(subcls, cls, expected):
-    assert issubclass(subcls, cls) == expected
+def test_dtype(dtype: Any, expected: Any) -> None:
+    assert get_dtype(DataArrayLike[dtype, Any]) == expected  # type: ignore
