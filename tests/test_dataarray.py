38aacd1daae34b6ee22ad80335d98a82412cff65
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-17 01:21:05 +0900

    #26 Update tests for dataarray module

--- tests/test_dataarray.py
+++ tests/test_dataarray.py
@@ -1,38 +1,51 @@
+# standard library
+from typing import Tuple
+
+
 # third-party packages
-from pytest import mark
-from xarray_dataclasses.dataarray import dataarrayclass, is_dataarrayclass
-from xarray_dataclasses.typing import DataArray
+import numpy as np
+import xarray as xr
+from typing_extensions import Literal
+
+
+# submodules
+from xarray_dataclasses.dataarray import dataarrayclass
+from xarray_dataclasses.typing import Attr, Coord, Data, Name
+
+
+# type hints
+X = Literal["x"]
+Y = Literal["y"]
 
 
 # test datasets
-DIMS = "x", "y", "z"
-
-
 @dataarrayclass
-class Base:
-    data: DataArray[DIMS[:2], float]
-    x: DataArray["x", int] = 0
-    y: DataArray["y", int] = 0
+class Image:
+    data: Data[float, Tuple[X, Y]]
+    x: Coord[int, X] = 0
+    y: Coord[int, Y] = 0
+    dpi: Attr[int] = 100
+    name: Name[str] = "image"
 
 
-@dataarrayclass
-class Extended(Base):
-    data: DataArray[DIMS, float]
-    z: DataArray["z", int] = 0
-
-
-class Invalid:
-    w: DataArray["w", int] = 0
-
-
-testdata = [
-    (Base, True),
-    (Extended, True),
-    (Invalid, False),
-]
+expected = xr.DataArray(
+    np.ones([10, 10], float),
+    dims=("x", "y"),
+    coords={
+        "x": ("x", np.zeros(10)),
+        "y": ("y", np.zeros(10)),
+    },
+    attrs={"dpi": 100},
+    name="image",
+)
 
 
 # test functions
-@mark.parametrize("cls, expected", testdata)
-def test_isdataarrayclass(cls, expected):
-    assert is_dataarrayclass(cls) == expected
+def test_dataarrayclass():
+    dataarray = Image.ones([10, 10])
+
+    assert (dataarray == expected).all()
+    assert dataarray.dtype == expected.dtype
+    assert dataarray.dims == expected.dims
+    assert dataarray.attrs == expected.attrs
+    assert dataarray.name == expected.name
