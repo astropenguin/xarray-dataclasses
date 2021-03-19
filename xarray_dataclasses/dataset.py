590dd5823675c48ee9337b0747e3bf65cb6b15b9
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-19 10:28:04 +0900

    #24 Update dataset module

--- xarray_dataclasses/dataset.py
+++ xarray_dataclasses/dataset.py
@@ -1,37 +1,32 @@
+from __future__ import annotations
+
+
 __all__ = ["asdataset", "datasetclass"]
 
 
 # standard library
 from dataclasses import dataclass
-from typing import Optional, Type
+from types import FunctionType
+from typing import Any, Callable, cast, Optional, Type, Union
 
 
 # third-party packages
 import xarray as xr
-from typing_extensions import Protocol
 
 
 # submodules
 from .common import (
-    ClassDecorator,
     DataClass,
     get_attrs,
     get_coords,
     get_data_vars,
 )
-from .utils import copy_wraps
-
-
-# type hints (internal)
-class DatasetClass(DataClass, Protocol):
-    """Type hint for a Dataset-class instance."""
-
-    new: classmethod
+from .utils import copy_wraps, extend_class
 
 
 # runtime functions (public)
-def asdataset(inst: DatasetClass) -> xr.Dataset:
-    """Create a Dataset instance from a Dataset-class instance."""
+def asdataset(inst: DataClass) -> xr.Dataset:
+    """Convert a Dataset-class instance to Dataset one."""
     dataset = xr.Dataset(get_data_vars(inst))
     coords = get_coords(inst, dataset)
 
@@ -50,22 +45,22 @@ def datasetclass(
     order: bool = False,
     unsafe_hash: bool = False,
     frozen: bool = False,
-) -> ClassDecorator[DatasetClass]:
+    shorthands: bool = True,
+) -> Union[Type[DataClass], Callable[[type], Type[DataClass]]]:
     """Class decorator to create a Dataset class."""
 
-    set_options = dataclass(
-        init=init,
-        repr=repr,
-        eq=eq,
-        order=order,
-        unsafe_hash=unsafe_hash,
-        frozen=frozen,
-    )
+    def to_dataclass(cls: type) -> Type[DataClass]:
+        if shorthands:
+            cls = extend_class(cls, DatasetMixin)
 
-    def to_dataclass(cls: type) -> Type[DatasetClass]:
-        set_options(cls)
-        set_shorthands(cls)
-        return cls
+        return dataclass(
+            init=init,
+            repr=repr,
+            eq=eq,
+            order=order,
+            unsafe_hash=unsafe_hash,
+            frozen=frozen,
+        )(cls)
 
     if cls is None:
         return to_dataclass
@@ -73,18 +68,33 @@ def datasetclass(
         return to_dataclass(cls)
 
 
-# runtime functions (internal)
-def set_shorthands(cls: Type[DatasetClass]) -> None:
-    """Set shorthand methods to a Dataset class."""
+# mix-in class (internal)
+class DatasetMixin:
+    """Mix-in class that provides shorthand methods."""
 
-    @copy_wraps(cls.__init__)  # type: ignore
-    def new(cls, *args, **kwargs):
+    @classmethod
+    def new(
+        cls,
+        *args: Any,
+        **kwargs: Any,
+    ) -> xr.Dataset:
+        """Create a Dataset instance."""
+        cls = cast(Type[DataClass], cls)
         return asdataset(cls(*args, **kwargs))
 
-    new.__annotations__["return"] = xr.Dataset
-    new.__doc__ = (
-        "Create a Dataset instance. This is a shorthand for "
-        f"``asdataset({cls.__name__}(*args, **kwargs))``."
-    )
+    def __init_subclass__(cls) -> None:
+        """Update new() based on the dataclass definition."""
+        dataclass(
+            init=True,
+            repr=False,
+            eq=False,
+            order=False,
+            unsafe_hash=False,
+            frozen=False,
+        )(cls)
 
-    cls.new = classmethod(new)
+        init = cast(FunctionType, cls.__init__)
+        new = copy_wraps(init)(cls.new.__func__)  # type: ignore
+        new.__annotations__["return"] = xr.Dataset
+        new.__doc__ = "Create a Dataset instance."
+        cls.new = classmethod(new)  # type: ignore
