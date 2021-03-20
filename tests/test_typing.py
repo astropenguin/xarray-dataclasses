c78409a35c0e24b1a8ae668a1ffc9871c48a5a61
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-20 15:12:47 +0900

    #24 Update tests for typing module

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,9 +1,8 @@
 # standard library
-from typing import Any, Tuple
+from typing import Any, ForwardRef, Tuple
 
 
 # third-party packages
-import numpy as np
 from pytest import mark
 from typing_extensions import Literal
 
@@ -13,6 +12,8 @@ from xarray_dataclasses.typing import DataArrayLike, get_dims, get_dtype
 
 
 # type hints
+x = ForwardRef("x")
+y = ForwardRef("y")
 X = Literal["x"]
 Y = Literal["y"]
 
@@ -22,37 +23,27 @@ testdata_dims = [
     (None, ()),
     (Tuple[()], ()),
     (X, ("x",)),
-    ("x", ("x",)),
+    (x, ("x",)),
     (Tuple[X], ("x",)),
-    (Tuple["x"], ("x",)),  # noqa
+    (Tuple[x], ("x",)),  # type: ignore
     (Tuple[X, Y], ("x", "y")),
-    (Tuple["x", "y"], ("x", "y")),  # noqa
+    (Tuple[x, y], ("x", "y")),  # type: ignore
 ]
 
 testdata_dtype = [
     (Any, None),
-    (bool, np.bool_),
-    (bytes, np.bytes_),
-    (str, np.str_),
-    (int, np.int64),
-    (float, np.float64),
-    (complex, np.complex128),
-    ("bool", np.bool_),
-    ("bytes", np.bytes_),
-    ("str", np.str_),
-    ("int", np.int64),
-    ("float", np.float64),
-    ("complex", np.complex128),
-    ("datetime64", np.datetime64),
+    (int, int),
+    ("int", "int"),
+    (Literal["int"], "int"),
 ]
 
 
 # test functions
 @mark.parametrize("dims, expected", testdata_dims)
 def test_dims(dims: Any, expected: Any) -> None:
-    assert get_dims(DataArrayLike[Any, dims]) == expected  # type: ignore
+    assert get_dims(DataArrayLike[Any, dims]) == expected
 
 
 @mark.parametrize("dtype, expected", testdata_dtype)
 def test_dtype(dtype: Any, expected: Any) -> None:
-    assert get_dtype(DataArrayLike[dtype, Any]) == expected  # type: ignore
+    assert get_dtype(DataArrayLike[dtype, Any]) == expected
