9f864c67817b8052b29eff3dbd42854050a542d1
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-08-08 02:02:24 +0900

    #55 Add parser functions for dims and dtype

--- xarray_dataclasses/parser.py
+++ xarray_dataclasses/parser.py
@@ -1,16 +1,70 @@
 # standard library
-from typing import TypeVar
+from itertools import chain
+from typing import Any, ForwardRef, Optional, Tuple, TypeVar
 
 
 # third-party packages
-from typing_extensions import Annotated, get_args, get_origin
+from typing_extensions import Annotated, get_args, get_origin, Literal
 
 
 # type hints
+Dims = Tuple[str, ...]
+Dtype = Optional[str]
+NoneType = type(None)
 T = TypeVar("T")
 
 
 # helper features
+def parse_dims(type_like: Any) -> Dims:
+    """Parse a type-like object and get dims."""
+    type_like = unannotate(type_like)
+
+    if type_like == () or type_like is NoneType:
+        return ()
+
+    if isinstance(type_like, ForwardRef):
+        return (type_like.__forward_arg__,)
+
+    if isinstance(type_like, str):
+        return (type_like,)
+
+    origin = get_origin(type_like)
+    args = get_args(type_like)
+
+    if origin is tuple:
+        return tuple(chain(*map(parse_dims, args)))
+
+    if origin is Literal:
+        return tuple(map(str, args))
+
+    raise ValueError(f"Could not parse {type_like}.")
+
+
+def parse_dtype(type_like: Any) -> Dtype:
+    """Parse a type-like object and get dtype."""
+    type_like = unannotate(type_like)
+
+    if type_like is Any or type_like is NoneType:
+        return None
+
+    if isinstance(type_like, type):
+        return type_like.__name__
+
+    if isinstance(type_like, ForwardRef):
+        return type_like.__forward_arg__
+
+    if isinstance(type_like, str):
+        return type_like
+
+    origin = get_origin(type_like)
+    args = get_args(type_like)
+
+    if origin is Literal and len(args) == 1:
+        return str(args[0])
+
+    raise ValueError(f"Could not parse {type_like}.")
+
+
 def unannotate(obj: T) -> T:
     """Recursively remove Annotated types."""
     if get_origin(obj) is Annotated:
