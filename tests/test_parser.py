2a225b4355a50bff36e713ea9cbd49e46099d867
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-08-21 20:04:27 +0900

    #59 Update tests for parser module

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -34,25 +34,22 @@ structure = parse(Image)
 def test_attr() -> None:
     assert structure.attr[0].name == "dpi"
     assert structure.attr[0].value == 100
-    assert structure.attr[0].type == int
+    assert structure.attr[0].type == "int"
 
 
 def test_coord() -> None:
     assert structure.coord[0].name == "x"
-    assert structure.coord[0].dims == ("x",)
-    assert structure.coord[0].dtype == "int"
+    assert structure.coord[0].type == {"dims": ("x",), "dtype": "int"}
     assert structure.coord[1].name == "y"
-    assert structure.coord[1].dims == ("y",)
-    assert structure.coord[1].dtype == "int"
+    assert structure.coord[1].type == {"dims": ("y",), "dtype": "int"}
 
 
 def test_data() -> None:
     assert structure.data[0].name == "data"
-    assert structure.data[0].dims == ("x", "y")
-    assert structure.data[0].dtype == "float"
+    assert structure.data[0].type == {"dims": ("x", "y"), "dtype": "float"}
 
 
 def test_name() -> None:
     assert structure.name[0].name == "name"
     assert structure.name[0].value == "image"
-    assert structure.name[0].type == str
+    assert structure.name[0].type == "str"
