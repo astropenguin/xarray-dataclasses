f09d7785444588d4c18a668307667614ffe312ee
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-09-13 15:47:20 +0000

    #71 Refactor unannotate function

--- xarray_dataclasses/typing.py
+++ xarray_dataclasses/typing.py
@@ -310,21 +310,22 @@ def get_dtype(type_like: Any) -> Dtype:
 
 def unannotate(obj: T) -> T:
     """Recursively remove Annotated types."""
-    if get_origin(obj) is Annotated:
-        obj = get_args(obj)[0]
+    import typing
 
+    args = get_args(obj)
     origin = get_origin(obj)
 
     if origin is None:
         return obj
 
-    args = map(unannotate, get_args(obj))
+    if origin is Annotated:
+        return unannotate(args[0])
+
+    args = map(unannotate, args)
     args = tuple(filter(None, args))
 
     try:
         return origin[args]
     except TypeError:
-        import typing
-
         name = origin.__name__.capitalize()
         return getattr(typing, name)[args]
