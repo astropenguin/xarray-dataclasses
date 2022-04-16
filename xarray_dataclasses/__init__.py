f15d46ba41fa610ab9784c24e24cb64c27c37b3f
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-04-16 12:39:02 +0000

    #154 Add empty dataspec module and test

--- xarray_dataclasses/__init__.py
+++ xarray_dataclasses/__init__.py
@@ -7,6 +7,7 @@ from . import dataarray
 from . import dataset
 from . import datamodel
 from . import dataoptions
+from . import dataspec
 from . import typing
 
 
@@ -15,6 +16,7 @@ from .dataarray import *
 from .dataset import *
 from .datamodel import *
 from .dataoptions import *
+from .dataspec import *
 from .typing import *
 
 
