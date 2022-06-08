53cec78b804754b7a9bc2e83fafda3eb9c7b484b
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-06-08 15:14:02 +0000

    #160 Update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,30 @@
-# Project information
+# project information
 author = "Akio Taniguchi"
 copyright = "2020-2022 Akio Taniguchi"
 
 
-# General configuration
+# general configuration
+add_module_names = False
+autodoc_typehints = "both"
+autodoc_typehints_format = "short"
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
 extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
-# Options for HTML output
-html_static_path = ["_static"]
+# options for HTML output
 html_logo = "_static/logo.svg"
+html_static_path = ["_static"]
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "github_url": "https://github.com/astropenguin/xarray-dataclasses/",
