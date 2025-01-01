a059250552c66f629a1221ce7b84bea3ff11be21
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2025-01-01 13:22:51 +0000

    #237 Add typing module

--- xarray_dataclasses/__init__.py
+++ xarray_dataclasses/__init__.py
@@ -1,1 +1,19 @@
+__all__ = [
+    # submodules
+    "typing",
+    # aliases
+    "Attr",
+    "Coord",
+    "Data",
+    "Multiple",
+    "Tag",
+]
 __version__ = "2.0.0"
+
+
+# submodules
+from . import typing
+
+
+# aliases
+from .typing import *
